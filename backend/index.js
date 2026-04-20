d9129f88ea5b86ab0e7e916daf9482082ea5bd13
Alex Abraham <alexab6104@gmail.com>
2026-04-20 17:00:30 -0400

    Configured PostgreSQL connection pool using environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,4 +17,13 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
+})
+
+// Testing the db connection (PostgreSQL)
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to the database:', err)
+  } else {
+    console.log('Database connection successful:', res.rows[0])
+  }
 })
